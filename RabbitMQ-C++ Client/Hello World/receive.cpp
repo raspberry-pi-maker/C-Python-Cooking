0e3c2016457e604fb527d9d87d27df074074750f
Seunghyun Lee <raspberry.pi.maker@gmail.com>
2022-12-26 01:38:35 +0900

    update

---
+++ RabbitMQ-C++ Client/Hello World/receive.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
+
+using namespace std;
+using namespace AmqpClient;
+
+bool g_loop = true;
+void sig_handler(int signo)
+{
+    if (SIGINT == signo) g_loop = false;
+}
+
+int main(int argc, char * argv[])
+{
+    struct sigaction act;
+    act.sa_handler = sig_handler;
+    sigaction(SIGINT, &act, NULL);
+
+    Channel::ptr_t channel = Channel::Create("192.168.150.128");
+    channel->DeclareQueue("hello", false, false, false, false);
+    channel->BasicConsume("hello", "hello", true, true, false);
+    //channel->BasicConsume("hello","hello");
+    cout << " [*] Waiting for messages. To exit press Ctrl + C" <<endl;
+    while(g_loop){
+        try{
+            Envelope::ptr_t envelope;
+            bool bflag = channel->BasicConsumeMessage("hello", envelope, 1000);
+            if(bflag == false){  //time out
+                fprintf(stderr, ".");
+                continue;
+            }
+            
+            fprintf(stderr, " [x] Received %s\n",envelope->Message()->Body().c_str() );
+        }
+        catch(MessageReturnedException &e){
+            fprintf(stderr, " Message receive error\n" );
+        }
+    }
+    cout << "\n Goob Bye" << endl;    
+    return 0;
+}
